4b1eee6286c7d37fb380a7f271402c4834542a8b
MoonLGH <66781351+MoonLGH@users.noreply.github.com>
2021-01-21 20:57:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const ytcm = /\S+\/ytdl (.+)/
 // Matches "/Download [whatever]"
 
 let baseUrl = "https://otakudesu.tv/"
-bot.onText(/\/otakudesuhome (.+)/i, async(msg, match) => {
 const axios = require("axios")
 const cheerio = require("cheerio")
+bot.onText(/\/otakudesuhome (.+)/i, async(msg, match) => {
+
   let home = {};
   let ongoing = [];
   let complete = [];      
