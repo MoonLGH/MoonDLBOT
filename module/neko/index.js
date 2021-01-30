f3ae1adfae4c179a3f14c02d3bf87969cecc3d66
MoonLGH <66781351+MoonLGH@users.noreply.github.com>
2021-01-30 21:10:28 +0700

    Update index.js

--- module/neko/index.js
+++ module/neko/index.js
@@ -1,26 +1,27 @@
 exports.nekosearch = (chatId,resp,bot)=>{
-    const axios = require("axios")
+const axios = require("axios")
 const cheerio = require("cheerio")
 const request = require("request")
-const respsplit = resp.split(" ").join("+")
+const respsplit = resp.replace(" ", "+")
 
 const search = `https://nekopoi.care/?s=${respsplit}&post_type=anime`
-let hlist = []
-        let title,link;
+const hlist = []
+console.log(search)
+
     request(search, async (error, response, html) => {
-        let $ = cheerio.load(html);
-    $(".result").find("ul > li").each(function (){
-        title = $(this).find(".top > h2").text();
-        link = $(this).find(".top > h2").attr("href");
-        hlist.push({
-            title,
-            link
-          })
-        console.log(title , link)
+        // let $ = cheerio.load(html);
+        setTimeout(() => {
+        console.log(html)
+            
+        }, 1000);
+
+    // $(".result").find("ul > li").each(function (){
+    //     let title,link;
+    //     title = $(this).find(".top > h2").text();
+    //     link = $(this).find(".top > h2").attr("href");
+    //     hlist.push({
+    //         title,
+    //         link
+    //       })
     })
-        console.log(hlist)
-                console.log(title , link)
-})
-console.log(hlist)
-        console.log(title , link)
 }
